9d223ddffb438b4bc8409a3039c38e0d863bb604
Sean Norwood <norwood.sean@gmail.com>
2022-03-06 00:49:39 +0000

    chore: enable PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const withPWA = require("next-pwa")
+const runtimeCaching = require('next-pwa/cache')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    runtimeCaching
+  }
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
