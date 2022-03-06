f95db19572162c62783c102a8e5199a4bd32eb9e
Sean <norwood.sean@gmail.com>
2022-03-06 04:52:27 +0000

    chore: add privacy page

---
+++ src/pages/privacy.tsx
@@ -0,0 +1,13 @@
+import { Text } from "@nextui-org/react";
+
+export default function PrivacyPage() {
+  return (
+    <div>
+      <Text h4>
+        This app stores a single cookie. It's used to store the token from the
+        Hello Fresh API which is required by Hello Fresh to query for recipes.
+        No personal data is transmitted to or from this app.
+      </Text>
+    </div>
+  );
+}
