429cca9f823fa65087a65ed15a3f3176b62baea2
Sean Norwood <norwood.sean@gmail.com>
2022-03-14 00:04:25 -0500

    chore: make recipe cards easier to read
    Adds recipe tags to card footer

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,4 +1,12 @@
-import { Card, Grid, Link, Text } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  Container,
+  Grid,
+  Link,
+  Row,
+  Text,
+} from "@nextui-org/react";
 import { Item } from "../recipes";
 
 export const RecipeCard = ({ recipe }: { recipe: Item }) => {
@@ -6,16 +14,29 @@ export const RecipeCard = ({ recipe }: { recipe: Item }) => {
     <Grid xs={12} sm={3}>
       <Link rel="noreferrer" target="_blank" href={recipe.websiteUrl}>
         <Card cover hoverable clickable>
-          <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
-            <Text weight="bold" h4 color="black">
-              {recipe?.name}
-            </Text>
-          </Card.Header>
           <Card.Image
             width="100%"
             height={340}
             src={`https://img.hellofresh.com/hellofresh_s3${recipe?.imagePath}`}
           />
+          <Card.Footer>
+            <Container>
+              <Row>
+                <Text weight="bold" h4 color="black">
+                  {recipe?.name}
+                </Text>
+              </Row>
+              {recipe.tags.length > 0 && (
+                <Grid.Container>
+                  {recipe.tags.map((tag) => (
+                    <Grid xs={4} sm={3} key={tag.id}>
+                      <Button size="xs">{tag.name}</Button>
+                    </Grid>
+                  ))}
+                </Grid.Container>
+              )}
+            </Container>
+          </Card.Footer>
         </Card>
       </Link>
     </Grid>
