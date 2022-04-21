82a9bef97a88724ae791a2033805fa9ded4e3e92
Sean Norwood <norwood.sean@gmail.com>
2022-04-21 19:27:58 +0000

    chore: add pre commit hooks

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -4,9 +4,9 @@ export default function PrivacyPage() {
   return (
     <div>
       <Text h4>
-        This app stores a single cookie. It&apos;s used to store the token from the
-        Hello Fresh API which is required by Hello Fresh to query for recipes.
-        No personal data is transmitted to or from this app.
+        This app stores a single cookie. It&apos;s used to store the token from
+        the Hello Fresh API which is required by Hello Fresh to query for
+        recipes. No personal data is transmitted to or from this app.
       </Text>
     </div>
   );
