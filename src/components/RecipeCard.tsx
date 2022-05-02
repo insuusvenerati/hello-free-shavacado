56c337061929aeddaacda7ec6ee5c225e8460acd
Sean Norwood <norwood.sean@gmail.com>
2022-05-02 12:41:31 +0000

    feat(ui): adds placeholder images to recipe cards

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,7 +1,8 @@
 import { Badge, Card, Text } from "@mantine/core";
 import Image from "next/image";
-import { useCallback } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { Item } from "../types/recipes";
+import { getPlaceholder } from "../util/getPlaceholder";
 
 type Props = {
   recipe: Item;
@@ -13,16 +14,30 @@ export const RecipeCard = ({ recipe, handler, setSelectedRecipe }: Props) => {
   const selectedRecipeHandler = useCallback(() => {
     setSelectedRecipe(recipe);
   }, [setSelectedRecipe, recipe]);
+  const [placeholder, setPlaceholder] = useState("");
+
+  useEffect(() => {
+    getPlaceholder(
+      `https://img.hellofresh.com/hellofresh_s3${recipe?.imagePath}`,
+    )
+      .then((value) => setPlaceholder(value.base64))
+      .catch((err) => console.log(err));
+  }, [recipe?.imagePath]);
 
   return (
     <Card onClick={handler} onMouseEnter={selectedRecipeHandler} shadow="sm">
-      <Card.Section>
-        <Image
-          alt={recipe.name}
-          height={340}
-          src={`https://img.hellofresh.com/hellofresh_s3${recipe?.imagePath}`}
-          width={600}
-        />
+      {/** TODO Could be better. Style props bad */}
+      <Card.Section sx={{ width: 456.25, height: 258.5, marginBottom: 5 }}>
+        {placeholder && (
+          <Image
+            alt={recipe.name}
+            blurDataURL={placeholder}
+            height={340}
+            placeholder="blur"
+            src={`https://img.hellofresh.com/hellofresh_s3${recipe?.imagePath}`}
+            width={600}
+          />
+        )}
       </Card.Section>
 
       <Text weight="bold">{recipe?.name}</Text>
