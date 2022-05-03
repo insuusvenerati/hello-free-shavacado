7c2d820c56b108d1c910249e1884831a275d173f
Sean Norwood <norwood.sean@gmail.com>
2022-05-03 16:08:29 +0000

    chore: add opengraph images

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,17 +1,28 @@
+/**
+ * @type {import('next-seo').NextSeoProps}
+ */
 const openGraphConfig = {
   openGraph: {
-    type: 'website',
-    locale: 'en_US',
-    url: 'https://hf.stiforr.tech',
-    site_name: 'Hello Free Shavacado',
+    type: "website",
+    locale: "en_US",
+    url: "https://hf.stiforr.tech",
+    site_name: "Hello Free Shavacado",
     title: "Hello Free Shavacado",
-    description: "Search for Hello Fresh recipes by ingredient"
+    description: "Search for Hello Fresh recipes by ingredient",
+    images: [
+      {
+        url: "https://i.imgur.com/0RSkm23.jpg",
+        height: 1800,
+        width: 1200,
+        alt: "Hello Free Shavacado Logo",
+      },
+    ],
   },
   twitter: {
-    handle: '@stiforr',
-    site: '@stiforr',
-    cardType: 'summary_large_image',
+    handle: "@stiforr",
+    site: "@stiforr",
+    cardType: "app",
   },
-}
+};
 
-export default openGraphConfig
+export default openGraphConfig;
