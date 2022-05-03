76d497020a654a243fd8a02def53062cdf1d88d7
Sean Norwood <norwood.sean@gmail.com>
2022-05-03 15:45:47 +0000

    chore: update manifest and icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -33,6 +33,27 @@ const App = (props: AppProps) => {
           content="minimum-scale=1, initial-scale=1, width=device-width"
           name="viewport"
         />
+        <link
+          href="/apple-touch-icon.png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+        />
+        <link
+          href="/favicon-32x32.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href="/favicon-16x16.png"
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link href="/site.webmanifest" rel="manifest" />
+        <link color="#5bbad5" href="/safari-pinned-tab.svg" rel="mask-icon" />
+        <meta content="#da532c" name="msapplication-TileColor" />
+        <meta content="#f69435" name="theme-color"></meta>
       </Head>
       <ColorSchemeProvider
         colorScheme={colorScheme}
