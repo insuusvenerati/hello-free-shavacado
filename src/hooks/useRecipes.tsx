88503e2319c9936aef13a5645171d866d70a7a1a
Sean Norwood <norwood.sean@gmail.com>
2022-05-04 17:41:48 +0000

    chore(ui): extract recipe query and related logic

---
+++ src/hooks/useRecipes.tsx
@@ -0,0 +1,94 @@
+import { getCookie } from "cookies-next";
+import ky from "ky";
+import { useCallback, useState } from "react";
+import { useQuery } from "react-query";
+import { Item, RecipeQuery } from "../types/recipes";
+import { useDebounce } from "../util/useDebounce";
+import { useFilterRecipes } from "./useFilters";
+import { useRecipesQuery } from "./useRecipesQuery";
+
+export const useRecipes = () => {
+  const [selectedRecipe, setSelectedRecipe] = useState<Item>();
+  const [searchText, setSearchText] = useState("");
+  const debouncedSearchText = useDebounce<string>(searchText, 1000);
+  const token = getCookie("token");
+  const [page, setPage] = useState(1);
+
+  const {
+    data: recipes,
+    isLoading,
+    error,
+    isError,
+  } = useRecipesQuery({ debouncedSearchText, page, token });
+
+  const {
+    filteredRecipes,
+    recipesTotal,
+    selectedAllergens,
+    selectedIngredients,
+    setSelectedAllergens,
+    setSelectedIngredients,
+  } = useFilterRecipes(recipes);
+
+  const allergens = [
+    ...new Set(
+      recipes?.items
+        ?.map((item) => item.allergens.map((allergen) => allergen.name))
+        .flat(),
+    ),
+  ];
+
+  const ingredients = [
+    ...new Set(
+      recipes?.items
+        ?.map((recipe) =>
+          recipe.ingredients.map((ingredient) => ingredient.name),
+        )
+        .flat(),
+    ),
+  ];
+
+  const onChangeHandler = useCallback(
+    (event) => {
+      setSearchText(event.target.value);
+    },
+    [setSearchText],
+  );
+
+  const pageChangeHandler = useCallback((pageNumber: number) => {
+    setPage(pageNumber);
+  }, []);
+
+  const handleSetSelectedIngredients = useCallback(
+    (value: string[]) => {
+      setSelectedIngredients(value);
+    },
+    [setSelectedIngredients],
+  );
+
+  const handleSetSelectedAllergens = useCallback(
+    (value: string[]) => {
+      setSelectedAllergens(value);
+    },
+    [setSelectedAllergens],
+  );
+
+  return {
+    isLoading,
+    isError,
+    error,
+    filteredRecipes,
+    selectedRecipe,
+    recipesTotal,
+    page,
+    setSelectedRecipe,
+    onChangeHandler,
+    pageChangeHandler,
+    handleSetSelectedIngredients,
+    handleSetSelectedAllergens,
+    allergens,
+    ingredients,
+    selectedAllergens,
+    selectedIngredients,
+  };
+};
