cb16d87bd833f9877c598938fabb242b62e58381
Sean Norwood <norwood.sean@gmail.com>
2022-05-04 17:41:21 +0000

    chore(ui): extract appshell component

---
+++ src/components/MyAppShell.tsx
@@ -0,0 +1,91 @@
+import {
+  AppShell,
+  Burger,
+  Container,
+  Header,
+  MediaQuery,
+  MultiSelect,
+  Navbar,
+  Stack,
+  Text,
+} from "@mantine/core";
+import { NavbarContent } from "./NavContent";
+
+export const MyAppShell = ({ children, ...props }) => {
+  const {
+    matches,
+    handleDrawer,
+    opened,
+    allergens,
+    handleSetSelectedAllergens,
+    selectedAllergens,
+    ingredients,
+    handleSetSelectedIngredients,
+    selectedIngredients,
+  } = props;
+
+  return (
+    <AppShell
+      fixed
+      header={
+        !matches ? (
+          <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+            <Header height={70} p="md">
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
+                <Burger
+                  mr="xl"
+                  onClick={handleDrawer}
+                  opened={opened}
+                  size="sm"
+                />
+
+                <Text>Hello Free Shavacado</Text>
+              </div>
+            </Header>
+          </MediaQuery>
+        ) : undefined
+      }
+      navbar={
+        <Navbar
+          hidden={!opened}
+          hiddenBreakpoint="sm"
+          p="md"
+          width={{ base: 300 }}
+        >
+          <NavbarContent />
+          <Container p="md">
+            <Stack>
+              <MultiSelect
+                clearable
+                data={allergens}
+                label="Filter allergens"
+                onChange={handleSetSelectedAllergens}
+                placeholder="Select an allergen"
+                searchable
+                value={selectedAllergens}
+              />
+              <MultiSelect
+                clearable
+                data={ingredients}
+                label="Filter ingredients"
+                onChange={handleSetSelectedIngredients}
+                placeholder="Select your ingredients"
+                searchable
+                value={selectedIngredients}
+              />
+            </Stack>
+          </Container>
+        </Navbar>
+      }
+      navbarOffsetBreakpoint="sm"
+    >
+      {children}
+    </AppShell>
+  );
+};
