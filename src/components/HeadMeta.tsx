5cb13e6bba73845587475a9598cadeb7747434b4
Sean Norwood <norwood.sean@gmail.com>
2022-05-07 02:21:46 +0000

    chore: update head component

--- src/components/HeadMeta.tsx
+++ src/components/HeadMeta.tsx
@@ -1,10 +1,4 @@
-export const HeadMeta = ({
-  title,
-  description,
-}: {
-  title: string;
-  description: string;
-}) => (
+export const HeadMeta = ({ title }: { title: string }) => (
   <>
     <meta content={title} name="application-name" />
     <meta content="yes" name="apple-mobile-web-app-capable" />
@@ -19,16 +13,8 @@ export const HeadMeta = ({
 
     <link href="/touch-icon-iphone.png" rel="apple-touch-icon" />
     <link href="/touch-icon-ipad.png" rel="apple-touch-icon" sizes="152x152" />
-    <link
-      href="/touch-icon-iphone-retina.png"
-      rel="apple-touch-icon"
-      sizes="180x180"
-    />
-    <link
-      href="/touch-icon-ipad-retina.png"
-      rel="apple-touch-icon"
-      sizes="167x167"
-    />
+    <link href="/touch-icon-iphone-retina.png" rel="apple-touch-icon" sizes="180x180" />
+    <link href="/touch-icon-ipad-retina.png" rel="apple-touch-icon" sizes="167x167" />
 
     <link href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
     <link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
