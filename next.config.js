7dbc2996f22a7bc7f8be998953d87104af0f793c
Sean Norwood <norwood.sean@gmail.com>
2022-05-22 11:54:01 -0500

    chore: initial layer0 config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
+// This file was automatically added by layer0 init.
+// You should commit this file to source control.
+const { withLayer0, withServiceWorker } = require("@layer0/next/config");
 const { withSentryConfig } = require("@sentry/nextjs");
-const { withPlaiceholder } = require("@plaiceholder/next");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -20,21 +20,34 @@ const nextConfig = {
     );
     return config;
   },
+  target: "server",
   experimental: {
     outputStandalone: true,
   },
+  outputFileTracing: true,
   images: {
     domains: ["img.hellofresh.com"],
   },
   reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    runtimeCaching,
-    disable: process.env.NODE_ENV === "development",
-  },
+  // pwa: {
+  //   dest: "public",
+  //   runtimeCaching,
+  //   disable: process.env.NODE_ENV === "development",
+  // },
   typescript: {
     ignoreBuildErrors: false,
   },
 };
 
-module.exports = withPlaiceholder(withPWA(withSentryConfig(nextConfig)));
+const _preLayer0Export = nextConfig;
+
+module.exports = (phase, config) =>
+  withLayer0(
+    withServiceWorker({
+      // Output sourcemaps so that stack traces have original source filenames and line numbers when tailing
+      // the logs in the Layer0 developer console.
+      layer0SourceMaps: false,
+
+      ..._preLayer0Export,
+    }),
+  );
