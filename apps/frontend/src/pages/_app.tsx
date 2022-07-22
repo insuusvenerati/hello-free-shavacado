a0bd3cb1bee3c2dd99e58b0eedcf1057326dd636
Sean Norwood <norwood.sean@gmail.com>
2022-07-22 00:55:08 -0500

    refactor(frontend): dont show nprogress spinner

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -55,6 +55,7 @@ const App = ({ Component, pageProps }: AppProps<CustomPageProps>) => {
   );
 
   useEffect(() => {
+    NProgress.configure({ showSpinner: false });
     Router.events.on("routeChangeStart", NProgress.start);
     Router.events.on("routeChangeComplete", NProgress.done);
     Router.events.on("routeChangeError", NProgress.done);
