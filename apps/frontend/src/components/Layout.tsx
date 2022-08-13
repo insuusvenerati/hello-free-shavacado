3f290c92e2fb84e28ec27d07e68c159ecdcbdeed
Sean Norwood <norwood.sean@gmail.com>
2022-08-13 13:31:39 -0500

    refactor(frontend): use algolia instead of meilisearch

--- apps/frontend/src/components/Layout.tsx
+++ apps/frontend/src/components/Layout.tsx
@@ -1,7 +1,7 @@
 import { Affix, Loader, Transition } from "@mantine/core";
 import { InstantSearch } from "react-instantsearch-hooks-web";
 import { useIsFetching } from "react-query";
-import { searchClient } from "util/meilisearch";
+import { algoliaSearch } from "util/algolia";
 import { RecipesProvider } from "../context/RecipesContext";
 import { MyAppShell } from "./MyAppShell";
 
@@ -14,7 +14,7 @@ export const Layout = ({ children }) => {
           {(transitionStyles) => <Loader styles={transitionStyles} />}
         </Transition>
       </Affix>
-      <InstantSearch routing indexName="hellofresh" searchClient={searchClient}>
+      <InstantSearch routing indexName="hellofresh" searchClient={algoliaSearch}>
         <MyAppShell>{children}</MyAppShell>
       </InstantSearch>
     </RecipesProvider>
