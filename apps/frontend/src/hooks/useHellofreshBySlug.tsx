54b6531867fd3331bc9f5196dc73ca7ed5de8025
Sean Norwood <norwood.sean@gmail.com>
2022-08-14 11:44:20 -0500

    refactor(frontend): add token to hellofresh search by slug in case it's needed

--- apps/frontend/src/hooks/useHellofreshBySlug.tsx
+++ apps/frontend/src/hooks/useHellofreshBySlug.tsx
@@ -1,3 +1,4 @@
+import { getCookie } from "cookies-next";
 import { useQuery } from "react-query";
 import { RecipeQuery } from "../types/recipes";
 import { hellofreshSearchBySlug } from "../util/hellofresh";
@@ -6,10 +7,11 @@ export const useHellofreshBySlug = (
   slug: string | string[] | undefined,
   initialData?: RecipeQuery,
 ) => {
+  const token = getCookie("hf-token") as string;
   return useQuery(
     ["hellofresh-by-slug", slug],
     async () => {
-      return await hellofreshSearchBySlug({ slug });
+      return await hellofreshSearchBySlug({ slug, token });
     },
     { enabled: !!slug, initialData: initialData },
   );
