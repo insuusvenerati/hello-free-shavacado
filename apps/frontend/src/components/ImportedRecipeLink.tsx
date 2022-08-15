9c41ec45df9640d234e5341a4c2b6313b7d6f7d3
Sean Norwood <norwood.sean@gmail.com>
2022-08-15 00:04:10 -0500

    refactor(frontend): update imported recipe link text style

--- apps/frontend/src/components/ImportedRecipeLink.tsx
+++ apps/frontend/src/components/ImportedRecipeLink.tsx
@@ -1,26 +1,30 @@
 import { LinkIcon, TrashIcon } from "@heroicons/react/outline";
-import { ActionIcon, Avatar, Group, List, Paper, Text, Tooltip } from "@mantine/core";
+import { ActionIcon, Avatar, createStyles, Group, List, Paper, Text, Tooltip } from "@mantine/core";
 import { useDeleteImportedRecipeMutation } from "../hooks/useDeleteImportedRecipeMutation";
 import { ImportedRecipe } from "../types/importedRecipe";
 import { CustomNextLink } from "./CustomNextLink";
 
+const useStyles = createStyles((theme) => ({
+  linkText: {
+    maxWidth: 100,
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    color: theme.colorScheme === "light" ? theme.primaryColor : "white",
+  },
+}));
+
 export const ImportedRecipeLink = ({ recipe }: { recipe: ImportedRecipe }) => {
+  const { classes } = useStyles();
   const { mutate } = useDeleteImportedRecipeMutation();
+
   return (
     <Paper mb="md" shadow="xs" withBorder>
       <List.Item icon={<Avatar alt={recipe?.name} radius={0} size="lg" src={recipe?.image} />}>
         <Group noWrap>
           <Tooltip label={recipe?.name} withArrow>
             <CustomNextLink href={recipe?.url} key={recipe?.id} target="_blank">
-              <Text
-                style={{
-                  maxWidth: 100,
-                  whiteSpace: "nowrap",
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                }}
-                size="sm"
-              >
+              <Text className={classes.linkText} size="sm">
                 {recipe?.name}
               </Text>
             </CustomNextLink>
